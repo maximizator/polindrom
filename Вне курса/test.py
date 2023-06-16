69b519a24332141d3f1654f0bb58a709d0c06dc1
maximizator <136617537+maximizator@users.noreply.github.com>
2023-06-16 11:55:36 +0300

    test.py

--- Вне курса/test.py
+++ Вне курса/test.py
@@ -1,6 +1,9 @@
-# git init
-# git add .
-# git commit -m "first commit"
-# git branch -M main
-# git remote add origin https://github.com/maximizator/polindrom.git
-# git push -u origin main
+def is_palindrome(s):
+    for i in range(len(s) // 2): # проходим до середины строки
+        if s[i] != s[-i-1]: # сравниваем символы с обоих концов строки
+            return False
+    return True
+
+
+s = input('Введите палиндром: ')
+print(is_palindrome(s))
